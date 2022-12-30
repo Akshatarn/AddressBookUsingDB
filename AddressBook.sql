42c5e88556879d7f2f3b445704c03d83a94c1eb5
akshata_rn <akshatarn123@gamail.com>
2022-12-30 21:59:51 +0530

    Table is created

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,3 +1,16 @@
 ---Creating database---
 CREATE DATABASE ADDRESSBOOK;
 
+---creating table--- 
+CREATE TABLE AddressBookTable
+(
+FirstName VARCHAR(25) NOT NULL,
+LastName VARCHAR(25) NOT NULL,
+Address VARCHAR(200) NOT NULL,
+City VARCHAR(25) NOT NULL,
+State VARCHAR(25) NOT NULL,
+Zip Int NOT NULL,
+PhoneNo Int NOT NULL,
+Email VARCHAR(30) NOT NULL
+);
+SELECT *FROM AddressBookTable;
